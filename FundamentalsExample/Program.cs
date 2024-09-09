e027c61e5dc7781fcd660605e3523439c690a48d
Matheus <matheus.resteves@gmail.com>
2024-09-09 14:48:35 -0300

    Feat: add project who uses other project in the solution

---
+++ FundamentalsExample/Program.cs
@@ -0,0 +1,11 @@
+﻿using FundamentalsExample.Common.Models;
+
+Person person1 = new Person("Matheus", 21);
+
+person1.SelfPresentation();
+
+/* 
+In this project the idea is simply to learn how to create a solution in .NET,
+grouping projects (FudamentalsExample.Common and FundamentalsExample) and making
+references to each other, using SolutionExample.sln
+*/
